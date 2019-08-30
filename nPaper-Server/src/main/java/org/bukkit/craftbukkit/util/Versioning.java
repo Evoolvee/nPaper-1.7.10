1d15dc8de63378ab91d22dd74a07805fdb2c3a5c
sathonay <sathonaypro@gmail.com>
2019-08-31 00:42:56 +0200

    Update Versioning.java

--- nPaper-Server/src/main/java/org/bukkit/craftbukkit/util/Versioning.java
+++ nPaper-Server/src/main/java/org/bukkit/craftbukkit/util/Versioning.java
@@ -11,7 +11,7 @@ public final class Versioning {
     public static String getBukkitVersion() {
         String result = "Unknown-Version";
 
-        InputStream stream = Bukkit.class.getClassLoader().getResourceAsStream("META-INF/maven/com.sathonay.npaper/paperspigot-api/pom.properties");
+        InputStream stream = Bukkit.class.getClassLoader().getResourceAsStream("META-INF/maven/com.sathonay.npaper/npaper-api/pom.properties");
         Properties properties = new Properties();
 
         if (stream != null) {
