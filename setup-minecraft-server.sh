350b2e1b4a47ac426666252806d44ca5332f96c5
Alexis <sathonaypro@gmail.com>
2021-04-19 18:33:32 +0200

    Remove zip folder after unziping

--- setup-minecraft-server.sh
+++ setup-minecraft-server.sh
@@ -1,4 +1,5 @@
 cd $HOME/.m2/repository
 curl https://download1480.mediafire.com/fjej4p9wqxzg/dtkp7ubl3zivph2/minecraft-server-repo.zip -O minecraft-server-repo.zip
 unzip -o minecraft-server-repo.zip
+rm minecraft-server-repo.zip
 echo "script ended \_(^^)_/"
