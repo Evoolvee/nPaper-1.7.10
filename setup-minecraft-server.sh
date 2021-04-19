60934d1a848cbb51b7b7dce4a5c369a8c94f5a28
sathonay <sathonaypro@gmail.com>
2021-04-19 07:32:38 +0200

    setup-minecraft-server.sh if minecraft-server repositories doesn't work, you can use that script to download the minecraft-server and add it in your local repositories.

---
+++ setup-minecraft-server.sh
@@ -0,0 +1,4 @@
+cd $HOME/.m2/repository
+curl https://download1480.mediafire.com/fjej4p9wqxzg/dtkp7ubl3zivph2/minecraft-server-repo.zip -O minecraft-server-repo.zip
+unzip -o minecraft-server-repo.zip
+echo "script ended \_(^^)_/"
