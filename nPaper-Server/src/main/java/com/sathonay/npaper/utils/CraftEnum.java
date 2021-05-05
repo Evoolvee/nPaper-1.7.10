1dc65b0c7b51e6741addc695395d2d9e4c87c39c
Alexis <33222808+sathonay@users.noreply.github.com>
2021-05-05 22:09:16 +0200

    Fix npe

--- nPaper-Server/src/main/java/com/sathonay/npaper/utils/CraftEnum.java
+++ nPaper-Server/src/main/java/com/sathonay/npaper/utils/CraftEnum.java
@@ -7,17 +7,17 @@ import org.bukkit.Difficulty;
 
 public class CraftEnum {
     public static EnumDifficulty getDifficulty(Difficulty difficulty) {
-        return EnumDifficulty.getById(difficulty.ordinal());
+        return difficulty == null ? null : EnumDifficulty.getById(difficulty.ordinal());
     }
 
     public static Difficulty getDifficulty(EnumDifficulty difficulty) {
-        return Difficulty.getByValue(difficulty.ordinal());
+        return difficulty == null ? null : Difficulty.getByValue(difficulty.ordinal());
     }
 
     public static ChatVisibility getChatVisibility(EnumChatVisibility visibility) {
-        return ChatVisibility.getByIndex(visibility.a());
+        return visibility == null ? null : ChatVisibility.getByIndex(visibility.a());
     }
     public static EnumChatVisibility getChatVisibility(ChatVisibility visibility) {
-        return EnumChatVisibility.a(visibility.ordinal());
+        return visibility == null ? null : EnumChatVisibility.a(visibility.ordinal());
     }
 }
